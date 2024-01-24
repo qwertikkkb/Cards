25f1f232fc2fed263f5eb219a5f8b1c7038e3f8d
qwertikkkb <147443772+qwertikkkb@users.noreply.github.com>
2024-01-24 23:34:40 +0200

    First Commit

---
+++ Cards/Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Nicolai Bodean on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
